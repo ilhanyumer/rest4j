f28bf007920e7c5448b75afb61eca9995a6be8a8
Ilhan Yumer <ilhan@ilhan.name>
2023-04-17 21:20:55 +0300

    Initial commit

---
+++ src/main/java/software/deve/rest4j/Rest4JApplication.java
@@ -0,0 +1,13 @@
+package software.deve.rest4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rest4JApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rest4JApplication.class, args);
+    }
+
+}
